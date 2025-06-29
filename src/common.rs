17c42a6c0a7d4d812184d0ccc3deacd271679861
makabakakk <k1211@dpsindo.net>
2025-06-29 17:50:44 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://38.247.64.116:21114".to_owned()
 }
 
 #[inline]
